29a2122dfe0d329a60f38fd8de51fb7f187ffdd7
Sami Wagiaalla <swagiaal@redhat.com>
2012-05-15 16:09:13 -0400

    Test AbstractKojiHubBaseClient.getRepo()

---
+++ org.fedoraproject.eclipse.packager.koji.tests/src/org/fedoraproject/eclipse/packager/koji/api/TestAbstractKojiHubBaseClientTest.java
@@ -0,0 +1,69 @@
+package org.fedoraproject.eclipse.packager.koji.api;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.fedoraproject.eclipse.packager.koji.api.errors.KojiHubClientException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAbstractKojiHubBaseClientTest {
+
+	@Before
+	protected void setUp() {
+	}
+
+	@After
+	protected void tearDown() {
+	}
+
+	@Test
+	public void testGetRepo() throws KojiHubClientException,
+			MalformedURLException {
+		final String tag = "f18-build";
+
+		final HashMap<String, Object> testMap = new HashMap<String, Object>();
+		testMap.put("id", 55);
+		testMap.put("creation_time", "yesterday");
+
+		ArrayList<Object> params = new ArrayList<Object>();
+		params.add(tag);
+
+		// Mock xmlRpcClient
+		final XmlRpcClient mockXmlRpcClient = new XmlRpcClient() {
+			@Override
+			public Object execute(String methodName,
+					@SuppressWarnings("rawtypes") List params) {
+				if (methodName.equals("getRepo") && params.get(0).equals(tag))
+					return testMap;
+
+				return null;
+			}
+		};
+
+		// Create a koji client using the mock xmlRpcClient
+		AbstractKojiHubBaseClient mockKojiClient = new AbstractKojiHubBaseClient(
+				"http://example.com") {
+			public HashMap<?, ?> login() {
+				return null;
+			}
+
+			protected void setupXmlRpcClient() {
+				xmlRpcClient = mockXmlRpcClient;
+			}
+		};
+
+		// Test getRepo
+		KojiRepoInfo info = mockKojiClient.getRepo(tag);
+		KojiRepoInfo testInfo = new KojiRepoInfo(testMap);
+		assertTrue("Repository information was retrieved correctly",
+				info.equals(testInfo));
+	}
+
+}
