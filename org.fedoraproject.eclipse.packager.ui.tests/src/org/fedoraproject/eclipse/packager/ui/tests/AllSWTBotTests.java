9a96592833d9032c1a65452145966849b8705080
Sami Wagiaalla <swagiaal@redhat.com>
2012-05-30 13:21:36 -0400

    Remove AddSourcesSWTBotTest

--- org.fedoraproject.eclipse.packager.ui.tests/src/org/fedoraproject/eclipse/packager/ui/tests/AllSWTBotTests.java
+++ org.fedoraproject.eclipse.packager.ui.tests/src/org/fedoraproject/eclipse/packager/ui/tests/AllSWTBotTests.java
@@ -16,7 +16,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	AddSourcesSWTBotTest.class,
 	DistGitImportSWTBotTest.class,
 	BodhiUpdateSWTBotTest.class,
 	DownloadSourcesSWTBotTest.class,
