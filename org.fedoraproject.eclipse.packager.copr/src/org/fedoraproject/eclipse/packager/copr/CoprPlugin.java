f53e1eb2f0c2e7d2e3892fb1764026fc76a55b48
Michael Simacek <msimacek@redhat.com>
2014-07-25 14:48:58 +0200

    Initial project skeleton for Copr plugin

---
+++ org.fedoraproject.eclipse.packager.copr/src/org/fedoraproject/eclipse/packager/copr/CoprPlugin.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.fedoraproject.eclipse.packager.copr;
+
+/**
+ * Plug-in responsible for managing Copr projects and building packages in Copr
+ */
+public class CoprPlugin {
+
+	/**
+	 * The plug-in ID.
+	 */
+	public static final String PLUGIN_ID = "org.fedoraproject.eclipse.packager.copr"; //$NON-NLS-1$
+}
